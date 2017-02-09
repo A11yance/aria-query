844d58400903d188dd6c0b2b2dda79abf9645adb
Jesse Renee Beach <splendidnoise@gmail.com>
2017-02-09 10:25:37 -0800

    Adding ariaMap test

---
+++ __tests__/src/ariaMap-test.js
@@ -0,0 +1,12 @@
+/* eslint-env mocha */
+import expect from 'expect';
+import rolesMap from '../../src/ariaMap';
+
+describe('ariaMap', function () {
+  it('should be a Map', function () {
+    expect(ariaMap).toBeA(Map);
+  });
+  it('should have size', function () {
+    expect(ariaMap.size).toBeGreaterThan(0);
+  });
+});
