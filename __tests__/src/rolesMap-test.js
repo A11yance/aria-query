b37af770128d75728abc92e08a5838cdf978c68c
Takeshi Kurosawa <kurosawa-takeshi@mitsue.co.jp>
2019-01-11 08:39:41 +0900

    Add double-check tests ensure that
    - role definitions use only props defined by props definitions
    - props are used in one or more role definitions
    
    See https://github.com/A11yance/aria-query/issues/18

--- __tests__/src/rolesMap-test.js
+++ __tests__/src/rolesMap-test.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 import expect from 'expect';
 import rolesMap from '../../src/rolesMap';
+import ariaPropsMap from '../../src/ariaPropsMap';
 
 describe('rolesMap', function () {
   it('should be a Map', function () {
@@ -9,4 +10,15 @@ describe('rolesMap', function () {
   it('should have size', function () {
     expect(rolesMap.size).toBeGreaterThan(0);
   });
+
+  for (const [role, definition] of rolesMap) {
+    describe(role, function() {
+      it('should have only props defined in ariaPropsMap', function() {
+        const unknownProps = Object.keys(definition.props).filter((prop) => {
+          return !ariaPropsMap.has(prop);
+        });
+        expect(unknownProps).toEqual([]);
+      });
+    });
+  }
 });
