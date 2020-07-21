2925c14ae488c12d7fad3343c53d325ce0e595f5
marcosvega91 <marcomoretti0103@gmail.com>
2020-07-21 08:38:00 +0200

    fix(definition): add dfn tag in definition role

--- src/etc/roles/literal/definitionRole.js
+++ src/etc/roles/literal/definitionRole.js
@@ -14,6 +14,12 @@ const definitionRole: ARIARoleDefinition = {
   relatedConcepts: [
     {
       concept: {
+        name: 'dfn',
+      },
+      module: 'HTML',
+    },
+    {
+      concept: {
         name: 'dd',
       },
       module: 'HTML',
