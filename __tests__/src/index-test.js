e4b6d28df777c5e6f2a14fa3cdfba74bc3007e19
J. Renée Beach <splendidnoise@gmail.com>
2020-12-30 15:53:09 -0800

    Update coveralls, expect and flow-bin (#92)

--- __tests__/src/index-test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env mocha */
-import expect from 'expect';
-import index from '../../src/index';
-
-describe('index', function () {
-  it('should be true', function () {
-    expect(true).toBe(true);
-  });
-});
