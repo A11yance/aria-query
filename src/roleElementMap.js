ef49ee8db49f86868fb3204b7e48d4e0062e5bfa
Jesse Renee Beach <splendidnoise@gmail.com>
2021-09-24 13:59:31 -0700

    Remove spread operator from roleElementMap

--- src/roleElementMap.js
+++ src/roleElementMap.js
@@ -10,29 +10,23 @@ type RoleElementRelationMap = Map<ARIARoleDefintionKey, ARIARoleRelationConceptS
 
 const roleElementMap: RoleElementRelationMap = new Map([]);
 
-[...rolesMap.keys()]
-  .forEach((
-    key: ARIARoleDefintionKey,
-  ): void => {
-    const role = rolesMap.get(key);
-    if (role) {
-      [
-        ...role.baseConcepts,
-        ...role.relatedConcepts,
-      ]
-      .forEach((
-        relation: ARIARoleRelation,
-      ): void => {
-        if (relation.module === 'HTML') {
-          const concept = relation.concept;
-          if (concept) {
-            const relationConcepts = roleElementMap.get(key) || new Set([]);
-            relationConcepts.add(concept);
-            roleElementMap.set(key, relationConcepts);
-          }
+const keys = rolesMap.keys();
+
+for (let key: ARIARoleDefintionKey of keys) {
+  const role = rolesMap.get(key);
+  if (role) {
+    const concepts = [].concat(role.baseConcepts, role.relatedConcepts);
+    for (let relation: ARIARoleRelation of concepts) {
+      if (relation.module === 'HTML') {
+        const concept = relation.concept;
+        if (concept) {
+          const relationConcepts = roleElementMap.get(key) || new Set([]);
+          relationConcepts.add(concept);
+          roleElementMap.set(key, relationConcepts);
         }
-      });
+      }
     }
-  });
+  }
+}
 
 export default roleElementMap;
